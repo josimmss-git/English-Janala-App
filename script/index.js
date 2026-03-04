3923c164a2e5a275ebe3daeb6576949e53ead2f8
josimmss-git <josimmss2021@gmail.com>
2026-03-05 05:36:42 +0600

    heading and banner section is done accordig to figma file

---
+++ script/index.js
@@ -0,0 +1,35 @@
+const loadLessons = () => {
+  // promise of responce
+  fetch("https://openapi.programming-hero.com/api/levels/all")
+  .then(res => res.json())  //promise of json data
+  .then((json) => displayLesson(json.data));
+}
+
+
+const displayLesson = (lessons) => {
+  // 1.get the container & empty
+  const levelContainer = document.getElementById("level-container");
+  levelContainer.innerHTML = "";
+
+  // 2. get into every loadLessons
+  for (let lesson of lessons) {
+
+
+    // 3.create Element
+    const btnDiv = document.createElement("div");
+    console.log(lesson);
+
+    btnDiv.innerHTML =`
+  <button onclick="loadLevelWord(${lesson.level_no})" class="btn btn-outline btn-primary">
+  <i class="fa-solid fa-book-open"></i>lesson - ${lesson.level_no}
+  </button>
+  `;
+    
+    
+    // 4.append into container
+    levelContainer.append(btnDiv);
+
+  }
+};
+
+loadLessons();
