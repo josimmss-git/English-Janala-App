765c197dd452a8b418223ecada7b910a671186f9
josimmss-git <josimmss2021@gmail.com>
2026-03-12 00:50:55 +0600

    all button active and loaded ui is done according to the figma file

--- script/index.js
+++ script/index.js
@@ -18,6 +18,21 @@ const displayLevelWord = (words) => {
   const wordContainer = document.getElementById("word-container");
   wordContainer.innerHTML = "";
 
+
+  if (words.length == 0) {
+    wordContainer.innerHTML = `
+    
+    <div
+     class=" text-center col-span-full rounded-xl py-10 space-y-6 bangla-font">
+
+     <img class="mx-auto" src="./assets/alert-error.png" alt="">
+      <p class="text-xl font-medium text-gray-400">“এই lesson কোনো ভোকাবুলারি যুক্ত করা হয়নি।”</p>
+      <h2 class="font-bold text-3xl">নেক্সট Lesson এ যান</h2>
+
+    </div>
+    `;
+  }
+
 //   {
 //     "id": 102,
 //     "level_no": 2,
@@ -31,9 +46,9 @@ const displayLevelWord = (words) => {
     card.innerHTML = `
   <div class="bg-white rounded-xl shadow-sm text-center py-10 px-5 space-y-4">
 
-      <h2 class="font-bold text-2xl">${word.word}</h2>
+      <h2 class="font-bold text-2xl">${word.word ? word.word:"শব্দ পাওয়া যায়নি"} </h2>
       <p class="font-semibold">Meading/Pronouncialtion</p>
-      <div class="bangla-font text-2xl font-medium">"${word.meaning} / ${word.pronunciation}"</div>
+      <div class="bangla-font text-2xl font-medium">"${word.meaning ? word.meaning: "অর্থ পাওয়া যায়নি"} / ${word.pronunciation? word.pronunciation : "pronunciation not found"}"</div>
       <div class="flex justify-between items-center">
         <button class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]"><i class="fa-solid fa-circle-info"></i></button>
         <button class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]"><i class="fa-solid fa-volume-high"></i></button>
